8d7c34e3a3fb962f8897ddc2902ffab4ac63cdcf
nandirides <nandiride@gmail.com>
2026-08-28 14:18:22 +0530

    working on user profile

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from user import views
 urlpatterns = [
     
-    path('', views.UserDashboardView.as_view(), name='user-dashboard'),
+    path('', views.UserHomeView.as_view(), name='home'),
+    path('booking/', views.UserBookingView.as_view(), name='booking'),
+    path('payment/', views.UserPaymentView.as_view(), name='payment'),
+    path('myride/', views.UserMyRideView.as_view(), name='myride'),
+    path('dashboard/', views.UserDashboardView.as_view(), name='user-dashboard'),
+    path('editprofile/', views.EditProfileView.as_view(), name='editprofile'),
     path('login/', views.LogintypeView.as_view(), name='login'),
     path('signup/', views.UserSignupView.as_view(), name='signup'),
     # path('', views.UserListView.as_view(), name='user-list'),
@@ -12,4 +17,8 @@ urlpatterns = [
     # path('users/<int:pk>/delete/', views.usersDeleteView.as_view(), name='users-delete'),
     path('loginuser/', views.UserLoginView.as_view(), name='loginuser'),
     path('profile/', views.UserProfileView.as_view(), name='profile'),
+    path('forgetpassword/', views.ForgetPasswordView.as_view(), name='forgetpassword'),
+    path('resetpassword/', views.ResetPasswordView.as_view(), name='resetpassword'),
+    path('logintypes/', views.UserLoginTypesView.as_view(), name='logintypes'),
+    # path('dashboard/', views.UserSideBarView.as_view(), name='dashboard'),
 ]
